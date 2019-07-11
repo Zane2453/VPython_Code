7d619fe5100c77143a5b8e7241f3ad598eb17ebf
YuHsuan Hu <sandy89876@gmail.com>
2019-07-11 20:54:26 +0800

    empty config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-iottalk_ip = "http://140.113.215.18"
-ccm_port = "7788"
-csm_port = "9999"
-
 port = 8888
+
+iottalk_server = ""
+ccm_path = ""
+csm_path = ""
--- static/config.js
+++ static/config.js
@@ -1,6 +1,5 @@
-var iottalk_ip = "http://140.113.215.18";
-var ccm_port = "7788";
-var csm_port = "9999";
+var cyberphysic_server = "";
 
-var cyberphysic_server_ip = "http://140.113.215.18";
-var cyberphysic_server_port = "8888";
+var iottalk_server = "";
+var ccm_path = "";
+var csm_path = "";
