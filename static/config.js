3d7a3a2197d84b304afc66be6b6be8fec3be7e4c
胡育旋 <sandy89876@gmail.com>
2019-07-19 17:02:06 +0800

    Update config.js

--- static/config.js
+++ static/config.js
@@ -2,6 +2,4 @@ var iottalk_server = "https://hu.iottalk.tw"
 var ccm_path = "/ccm";
 var csm_path = "/csm";
 
-var iottalk_server = "";
-var ccm_path = "";
-var csm_path = "";
+var cyberphysic_server = "";  //ex: "https://hu.iottalk.tw"
