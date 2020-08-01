b5cbfe57b92e19522d753db9c226120fffea77f6
Zane <z8872453@yahoo.com.tw>
2020-08-01 11:29:05 +0800

    BallThrow2 Add Coordination

---
+++ vp/py/Ball-throw2-coordinate.py
@@ -0,0 +1,110 @@
+speed = 5
+def Speed(data):
+    global speed, init_value_box
+    if data != None:
+        speed = data[0]
+        init_value_box.text = 'Speed:' + str(round(speed,1))
+        balljump(speed)
+        
+# 設定
+def setup():
+    profile = {
+        'dm_name' : 'BallThrow4',
+        'idf_list': [],
+        'odf_list' : [[Speed, ['']]],
+    }
+    dai(profile)
+
+setup()
+
+# [[Speed, ['']]] 讀取感測器後會自動更新
+# 請勿修改上方程式碼
+g=9.8
+size = 0.7
+scene = display(width=600, height=500, background=vector(0.6,0.3,0.2), center=vector(-9,7,0))
+floor = box(length=24, height=0.5, width=4, color=color.green, pos=vector(-9,0,0))
+box(length=8, height=0.5, width=4, color=color.red, pos= vector(7,0,0) )
+box(length=8, height=0.5, width=4, color=color.green, pos= vector(15,0,0) )
+
+# Modified by Zane 2020/07/05
+axis = []
+labels = []
+init_value_box = label(pos=vec(0,15,0), text= 'Speed:' + str(round(speed,1)), height=15, border=10, font='monospace', color = color.white)
+axisInit()
+
+scene.range=16
+camera_x1=0.8
+camera_x2=0.0
+scene.forward=vector(camera_x1,camera_x2,-1)
+
+preloadAudio('Startup.wav')
+preloadAudio('chord.wav')
+preloadAudio('gj.wav')
+
+# Modified by Zane 2020/07/05
+def axisInit():
+    global axis, labels
+    a = -21
+    b = 0
+    c = 40
+    d = 10
+    axis.append(arrow(pos=vec(a,b,-2), axis=vec(c+5,0,0), shaftwidth= 0.1, color = color.white))
+    axis.append(arrow(pos=vec(a,b,-2), axis=vec(0,d+5,0), shaftwidth= 0.1, color = color.white))
+    for t in range(0,20):
+        axis.append(box(pos=vec(a + (t+1)*(c/20),b+d/2,-2), length=0.1, height=d, width=0.1))
+    
+    for j in range(0,10):
+        axis.append(box(pos=vec(a + c/2,b + (j+1)*(d/10),-2), length=c, height=0.1, width=0.1,color=color.gray(0.8)))
+    
+    for x in range(0,6):
+        num = str(x*(c/5))
+        labels.append(label(pos=vec(a + x*(c/5),b-2*d/20,-2), text = num, height = 20, border = 12, font = 'monospace', color = color.white, box = False))
+    
+    for y in range(0,6):
+        num = str(b+y*(d/5))
+        labels.append(label(pos=vec(a-2*c/40,b + y*(d/5),-2), text = num, height = 20, border = 12, font = 'monospace', color = color.white, box = False))
+
+
+def balljump(spd):
+    if (spd < 3): return 
+
+    ball = sphere(pos=vector(-24.5, 10.0, 0.0), radius=size, color=color.white)
+    ball.velocity = vector(spd, -1.0, 0.0)
+
+    dt = 0.003
+    gj_playFlag=1
+
+    def resetScene():
+        scene.background = vector(0.6, 0.3, 0.2)
+        
+    
+    def jump():
+        global gj_playFlag
+
+        #console.log(ball.pos.x, ball.pos.y)
+        if ball.pos.x < 15:
+          rate(1000, jump)
+        else:
+          ball.visible = False
+          gj_playFlag = 1 
+          return
+
+        if (ball.pos.x < 10 and ball.pos.x > 1.5) and ball.pos.y < 1 :
+            scene.background = vector(0,0,0)
+            if gj_playFlag: 
+                playAudio('gj.wav') 
+                gj_playFlag=0
+            rate(4, resetScene)
+		
+        previous_x = ball.pos.x
+        ball.pos = ball.pos + ball.velocity * dt
+
+        if ball.pos.y < size and ball.velocity.y < 0:
+            ball.velocity.y = - ball.velocity.y
+            playAudio('chord.wav')
+        else:
+            ball.velocity.y = ball.velocity.y - g * dt
+    
+    jump()
+
+balljump(speed)
